77ed8136f8e360e1e29a38b428da9ce42b4d3940
Lutz Mueller <lutz.mueller@ottogroup.com>
2021-06-21 10:44:29 +0200

    add application class

---
+++ src/main/java/net/lumue/filewalkerd/FilewalkerdApplication.kt
@@ -0,0 +1,12 @@
+
+package net.lumue.filewalkerd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FilewalkerdApplication
+
+fun main(args: Array<String>) {
+   runApplication<FilewalkerdApplication>(*args)
+}
