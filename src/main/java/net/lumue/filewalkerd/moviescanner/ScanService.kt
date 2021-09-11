a836bee987b3a24a1562dc89a929b42a50ae62dd
Lutz Mueller <lutz.mueller@ottogroup.com>
2021-09-11 20:55:10 +0200

    start work on rest api

---
+++ src/main/java/net/lumue/filewalkerd/moviescanner/ScanService.kt
@@ -0,0 +1,12 @@
+package net.lumue.filewalkerd.moviescanner
+
+import org.springframework.stereotype.Service
+
+@Service
+class ScanService {
+    fun startScan(path: String, fileHandlerId: String) : String {
+
+    return ""
+    }
+
+}
---
+++ src/main/java/net/lumue/filewalkerd/restapi/ScanController.kt
@@ -0,0 +1,32 @@
+package net.lumue.filewalkerd.restapi
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import net.lumue.filewalkerd.moviescanner.ScanService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@ExperimentalCoroutinesApi
+class ScanController(@Autowired val scanService : ScanService) {
+
+    @RequestMapping(
+        path = ["/scan"],
+        method = [RequestMethod.POST],
+        produces = ["application/json; charset=utf-8"]
+    )
+    fun handlePost(@RequestBody scanRequest:ScanRequest): ResponseEntity<String> {
+        return ResponseEntity.ok(scanService.startScan(scanRequest.path, scanRequest.fileHandlerId))
+    }
+
+    
+}
+
+class ScanRequest(val path:String,val fileHandlerId:String) {
+
+
+}
