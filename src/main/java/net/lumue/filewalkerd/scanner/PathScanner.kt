87fc7011bafef215f7f05891b8bc9594db8d83f9
lm <lm@lumue.net>
2024-01-21 21:07:17 +0100

    Refactor PathScanner in Filewalkerd project
    The commit refactored the PathScanner class in the Filewalkerd project by breaking down the responsibility of the file handler. This allows for improved code readability, maintainability, and potential for further extension. Also, it leverages Kotlin's coroutines to handle file processing more efficiently.

--- src/main/java/net/lumue/filewalkerd/scanner/PathScanner.kt
+++ src/main/java/net/lumue/filewalkerd/scanner/PathScanner.kt
@@ -1,28 +1,33 @@
 package net.lumue.filewalkerd.scanner
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.asCoroutineDispatcher
 import mu.KotlinLogging
 import java.io.File
+import java.util.concurrent.ForkJoinPool
 
 @ExperimentalCoroutinesApi
 open class PathScanner(
     private val fileFilter: (file: File) -> Boolean = { true },
     private val fileHandler: FileHandler
-)
-{
+) {
     private val logger = KotlinLogging.logger {}
     fun execute(path: String) {
         logger.info { "execute $path" }
         val processFiles = ProcessFiles(
             fileFilter,
-            handleFile = { f ->
-                fileHandler.handleFile(f)
-            })
+            handleFile = ::handleGivenFile,
+            context = ForkJoinPool.commonPool().asCoroutineDispatcher()
+        )
         processFiles.invoke(path)
         logger.info { "exit execute path: $path" }
     }
 
-    interface FileHandler {
+    private fun handleGivenFile(file: File) {
+        fileHandler.handleFile(file)
+    }
+
+    fun interface FileHandler {
         fun handleFile(file: File)
     }
 }
